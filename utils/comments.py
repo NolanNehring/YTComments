4b6ea3dbba001e37687da6a8c33dbe46a3ab7ef9
NolanNehring <129118038+NolanNehring@users.noreply.github.com>
2023-04-22 19:36:43 -0500

    Update comments.py

--- utils/comments.py
+++ utils/comments.py
@@ -1,22 +1,17 @@
 import csv
-import re
-import string
 from datetime import datetime as dt
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-import nltk
 import html
 from nltk.corpus import stopwords
 from cleaner import (remove_repeated_characters, separate_digit_text, slang_look_up, remove_extra_space,remove_url, remove_alphanumerics,remove_words_start_with)
 
-nltk.download('punkt')
-nltk.download('stopwords')
-addition_words = ['add', 'plus', 'insert', 'include', 'append', 'make']
+
+addition_words = ['add', 'insert', 'include']
 subtraction_words = ['remove', 'subtract', 'delete', 'eliminate', 'exclude', 'get rid of']
 question_words = ["what", "when", "where", "who", "why", "how", "question"]
 comments = []
 
 today = dt.today().strftime('%d-%m-%Y')
-# cd "C:\Users\Nolan Nehring\Documents\Comments"
 def clean_comment(comment):
     # Decode HTML-encoded characters
     comment = html.unescape(comment)
@@ -46,30 +41,17 @@ def check_words(comment, words_list):
     for word in words_list:
         if word in comment:
             return 1
-        print(word)
     return 0
 def is_question(comment):
     return int(comment.strip().endswith('?'))
-def process_comments(response_items, csv_output = False):
+def process_comments(response_items, video_title, csv_output=False):
     for res in response_items:
         if 'topLevelComment' in res['snippet'].keys():
             comment = res['snippet']['topLevelComment']['snippet']
             comment['parentId'] = None
             comment['commentId'] = res['snippet']['topLevelComment']['id']
+            comment['videoTitle'] = video_title  # add video title to comment dict
             comments.append(comment)
-        # #Handle replies
-        # if 'replies' in res.keys():
-        #     for reply in res['replies']['comments']:
-        #         comment = reply['snippet']
-        #         comment['commentId'] = reply['id']
-        #         comments.append(comment)
-        # #Handle non-replies
-        # else:
-        #     comment = {}
-        #     comment['snippet'] = res['snippet']['topLevelComment']['snippet']
-        #     comment['snippet']['parentId'] = None
-        #     comment['snippet']['commentId'] = res['snippet']['topLevelComment']['id']
-        #     comments.append(comment['snippet'])
     
     # Clean comments
     cleaned_comments = [clean_comment(comment['textDisplay']) for comment in comments]
@@ -87,21 +69,22 @@ def process_comments(response_items, csv_output = False):
     print(f"Finished processing {len(comments)} comments")
     return comments
 
+
 def make_csv(comments, channelID=None):
-    header = list(comments[0].keys()) + ['cleaned_comment', 'addition', 'subtraction', 'question', ]
+    header = list(comments[0].keys()) + ['cleaned_comment', 'addition', 'subtraction', 'question', 'maybe question']
     unique_comments = []
 
     if channelID:
-        filename = f'comments_{channelID}_{today}.csv'
+        filename = f'comments_{channelID}.csv'
     else:
-        filename = f'comments_{today}.csv'
+        filename = 'comments.csv'
 
     with open(filename, 'w', encoding='utf8', newline='') as f:
         writer = csv.DictWriter(f, fieldnames=header)
         writer.writeheader()
 
         for comment in comments:
-            cleaned_comment = clean_comment(comment['textDisplay'])
+            cleaned_comment = clean_comment(comment.get('textDisplay', ''))
             comment['cleaned_comment'] = cleaned_comment
             comment['addition'] = check_words(cleaned_comment, addition_words)
             comment['subtraction'] = check_words(cleaned_comment, subtraction_words)
@@ -111,7 +94,13 @@ def make_csv(comments, channelID=None):
             # Check if comment already exists in the list
             if comment not in unique_comments:
                 unique_comments.append(comment)
-                writer.writerow(comment)
 
-    print(f"Finished writing {len(unique_comments)} unique comments to {filename}")
+                # Write row with all keys in header, even if they are not present in comment
+                row = {key: comment.get(key, '') for key in header}
+                writer.writerow(row)
+
+    with open(filename, 'r', encoding='utf8') as f:
+        csv_data = f.read()
+
+    return csv_data
 
